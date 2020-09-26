89c6eea047e0e3bc8f7a03d788e53bc62211f590
Abraham Bekele <abb2007hu@gmail.com>
2020-09-26 17:25:51 +0300

    user route linting&formating

--- router/user/user_router.js
+++ router/user/user_router.js
@@ -1,86 +1,72 @@
-import { Router } from 'express';
-import { User, Address, Picture } from '../../sequelize/db/models';
+import { Router } from 'express'
+import { User, Address, Picture } from '../../sequelize/db/models'
+import { compareSync } from 'bcrypt'
 const router = Router()
-import { compareSync } from 'bcrypt';
 
 router.get('/:id', async (req, res) => {
-    try {
-        const id = parseInt(req.params.id);
-        const user = await User.findOne({
-            include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
-            where: { id: id }
-        });
-        res.send(user)
-    } catch (error) {
-        throw error;
-    }
+  const id = parseInt(req.params.id)
+  const user = await User.findOne({
+    include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
+    where: { id: id }
+  })
+  res.send(user)
 })
 
 router.post('', async (req, res) => {
-    let body = req.body;
-    let user = {}
-    console.log(body);
-    try {
-        user = await User.create(body)
-        if (body.address) {
-            let addressBody = body.address;
-            addressBody.userId = user.id;
-            user.address = await Address.create(addressBody);
-        }
-        res.send(user)
-    } catch (error) {
-        throw error;
-    }
-});
+  const body = req.body
+  let user = {}
+  console.log(body)
+  user = await User.create(body)
+  if (body.address) {
+    const addressBody = body.address
+    addressBody.userId = user.id
+    user.address = await Address.create(addressBody)
+  }
+  res.send(user)
+})
 
 router.put('', async (req, res, err) => {
-    const body = req.body;
-    let respones = { isSuccess: true, updatedRows: [], message: [] }
-    try {
-        if (body.id) {
-            console.log(body);
-            if (body.address) {
-                const rows = await Address.update(body.address, { where: { userId: body.id } });
-                if (rows > 0) {
-                    respones.updatedRows.push({ address: rows });
-                } else {
-                    respones.isSuccess = false;
-                    respones.message.push('Failed to UPDATE address information')
-                }
-                delete body.address;
-            }
-            if (body.password && body.oldPassword) {
-                const user = await User.findOne({ where: { id: body.id } })
-                const isEqual = compareSync(body.oldPassword, user.password)
-                delete body.oldPassword;
-                if (isEqual) {
-                    const rows = await User.update(body, { where: { id: body.id } });
-                    if (rows > 0) {
-                        respones.updatedRows.push({ user: rows });
-                    } else {
-                        respones.isSuccess = false;
-                        respones.message.push('Failed to UPDATE user information')
-                    }
-                }
-            }
-            else if(!(body.password || body.oldPassword)){
-                respones.isSuccess = false;
-                respones.message.push('Your old password is incorrect')
-            }
-            else if (body.firstName || body.lastName || body.email || body.username || body.phone || body.userType) {
-                const rows = await User.update(body, { where: { id: body.id } });
-                if (rows > 0) {
-                    respones.updatedRows.push({ user: rows });
-                } else {
-                    respones.isSuccess = false;
-                    respones.message.push('Failed to UPDATE user information')
-                }
-            }
-        }
-        res.send(respones)
-    } catch (error) {
-        throw error;
+  const body = req.body
+  const respones = { isSuccess: true, updatedRows: [], message: [] }
+  if (body.id) {
+    console.log(body)
+    if (body.address) {
+      const rows = await Address.update(body.address, { where: { userId: body.id } })
+      if (rows > 0) {
+        respones.updatedRows.push({ address: rows })
+      } else {
+        respones.isSuccess = false
+        respones.message.push('Failed to UPDATE address information')
+      }
+      delete body.address
     }
-});
+    if (body.password && body.oldPassword) {
+      const user = await User.findOne({ where: { id: body.id } })
+      const isEqual = compareSync(body.oldPassword, user.password)
+      delete body.oldPassword
+      if (isEqual) {
+        const rows = await User.update(body, { where: { id: body.id } })
+        if (rows > 0) {
+          respones.updatedRows.push({ user: rows })
+        } else {
+          respones.isSuccess = false
+          respones.message.push('Failed to UPDATE user information')
+        }
+      }
+    } else if (!(body.password || body.oldPassword)) {
+      respones.isSuccess = false
+      respones.message.push('Your old password is incorrect')
+    } else if (body.firstName || body.lastName || body.email || body.username || body.phone || body.userType) {
+      const rows = await User.update(body, { where: { id: body.id } })
+      if (rows > 0) {
+        respones.updatedRows.push({ user: rows })
+      } else {
+        respones.isSuccess = false
+        respones.message.push('Failed to UPDATE user information')
+      }
+    }
+  }
+  res.send(respones)
+})
 
-export default router;
+export default router
--- router/user/users_router.js
+++ router/user/users_router.js
@@ -1,59 +1,52 @@
-import { Router } from 'express';
-import { User, Address, Picture } from '../../sequelize/db/models';
+import { Router } from 'express'
+import { User, Address, Picture } from '../../sequelize/db/models'
 const router = Router()
 
 router.get('', async (req, res) => {
-    try {
-        const users = await User.findAll({
-            include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
-            offset: 0,
-            limit: 10
-        });
-        res.send(users)
-    } catch (error) {
-        throw error;
-    }
+  const users = await User.findAll({
+    include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
+    offset: 0,
+    limit: 10
+  })
+  res.send(users)
 })
 router.get('/:query', async (req, res, err) => {
-    const query = req.params.query;
-    const isQueryValid = !(new RegExp("[^a-zA-Z0-9&=@.]").test(query))
-    if (isQueryValid) {
-        const params = getParams(query)
-        const users = await User.findAll({
-            where: params.where,
-            include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
-            offset: (params.page - 1) * params.limit,
-            limit: params.limit
-        });
-        res.send(users)
-    }
-    else {
-        res.json({
-            error: {
-                name: "Bad Format",
-                message: "Invalid Request URL format",
-                stack: ""
-            }
-        })
-    }
-});
+  const query = req.params.query
+  const isQueryValid = !(new RegExp('[^a-zA-Z0-9&=@.]').test(query))
+  if (isQueryValid) {
+    const params = getParams(query)
+    const users = await User.findAll({
+      where: params.where,
+      include: [{ model: Address, as: 'address' }, { model: Picture, as: 'picture' }],
+      offset: (params.page - 1) * params.limit,
+      limit: params.limit
+    })
+    res.send(users)
+  } else {
+    res.json({
+      error: {
+        name: 'Bad Format',
+        message: 'Invalid Request URL format',
+        stack: ''
+      }
+    })
+  }
+})
 
-function getParams(query) {
-    let params = { 'page': 1, limit: 10, 'where': {} };
-    const temp = query.split("&");
-    temp.forEach((param) => {
-        const key = param.split("=")[0];
-        const value = param.split("=")[1];
-        if (key && value) {
-            if (key === "page" || key === "limit")
-                params[key] = parseInt(value)
-            else {
-                params['where'][key] = value
-            }
-        }
-    });
-    console.log(params);
-    return params;
+function getParams (query) {
+  const params = { page: 1, limit: 10, where: {} }
+  const temp = query.split('&')
+  temp.forEach((param) => {
+    const key = param.split('=')[0]
+    const value = param.split('=')[1]
+    if (key && value) {
+      if (key === 'page' || key === 'limit') { params[key] = parseInt(value) } else {
+        params.where[key] = value
+      }
+    }
+  })
+  console.log(params)
+  return params
 }
 
-export default router;
+export default router
