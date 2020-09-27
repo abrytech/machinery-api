0732ea92ae0d13cb14e0ece7cfa089a90bc9f912
Abraham Bekele <abb2007hu@gmail.com>
2020-09-27 23:39:35 +0300

    Initial commit

--- middleware/gmail.js
+++ middleware/gmail.js
@@ -1,10 +1,7 @@
 import nodemailer from 'nodemailer'
 import template from './email_template'
 // async..await is not allowed in global scope, must use a wrapper
-export default async function (name, to, key) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-
+export default async function (to, key) {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -14,12 +11,10 @@ export default async function (name, to, key) {
     auth: {
       user: 'abb2007hu@gmail.com', // generated ethereal user
       pass: '0922964576' // generated ethereal password
-      // user: 'elkadoshenterprise@gmail.com', // generated ethereal user
-      // pass: '!@#123Elkadosh' // generated ethereal password
     }
   })
+
   const body = template(key)
-  console.log(`::::::::::::> Name: ${name} Address To:  ${to} Activation Key: ${key} <::::::::::::::::`)
   // send mail with defined transport object
   transporter.sendMail({
     from: 'abb2007hu@gmail.com', // sender address
@@ -30,11 +25,8 @@ export default async function (name, to, key) {
   }).then((info) => {
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     console.log('Message sent: %s', info.messageId)
-    // Preview only available when sending through an Ethereal account
-    console.log('Message Envelope : %s', info.envelope)
     console.log('Message Accepted : %s', info.accepted)
   }).catch((err) => {
     console.log(err)
   })
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
