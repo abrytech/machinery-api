6c6fbe0eb8adcf4ef877ae07795079d37cd71a19
Abraham Bekele <abb2007hu@gmail.com>
2020-09-27 19:36:26 +0300

    sds

--- middleware/gmail.js
+++ middleware/gmail.js
@@ -1,5 +1,4 @@
 import nodemailer from 'nodemailer'
-import email from './email_template'
 // async..await is not allowed in global scope, must use a wrapper
 export default async function (name, to, key) {
   // Generate test SMTP service account from ethereal.email
@@ -19,9 +18,24 @@ export default async function (name, to, key) {
     }
   })
 
-  const body = email(key)
+  const body = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      <div class="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a class="btn btn-primary btn-lg" href="https://machinery-api.herokuapp.com/auth/confirmation/${key}" role="button">Confirm Account</a>
+        </div>
+  </body>
+  </html>`
   console.log(`::::::::::::> Name: ${name} Address To:  ${to} Activation Key: ${key} <::::::::::::::::`)
-  console.log(body)
   // send mail with defined transport object
   transporter.sendMail({
     from: 'abb2007hu@gmail.com', // sender address
