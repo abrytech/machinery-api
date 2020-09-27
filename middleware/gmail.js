1faaeb9c42f6cf835acc65c8da8d065d3654c14a
Abraham Bekele <abb2007hu@gmail.com>
2020-09-27 18:48:25 +0300

    sfsfs

--- middleware/gmail.js
+++ middleware/gmail.js
@@ -17,14 +17,33 @@ export default async function (name, to, key) {
       // pass: '!@#123Elkadosh' // generated ethereal password
     }
   })
+
+  const body = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a class="btn btn-primary btn-lg" href="https://machinery-api.herokuapp.com/auth/confirmation/${key}" role="button">Confirm Account</a>
+        </div>
+  </body>
+  </html>`
   console.log(`::::::::::::> Name: ${name} Address To:  ${to} Activation Key: ${key} <::::::::::::::::`)
   // send mail with defined transport object
   transporter.sendMail({
     from: 'abb2007hu@gmail.com', // sender address
     to: to, // list of receivers
     subject: 'Registration Confrimation', // Subject line
-    text: `https://machinery-api.herokuapp.com/auth/confirmation/${key}` // plain text body
-    // html: body // html body
+    // text:  // plain text body
+    html: body // html body
   }).then((info) => {
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     console.log('Message sent: %s', info.messageId)
