1b6d64de9c29ba25bd68b7fa3b45f314a1e1d444
Abraham Bekele <abb2007hu@gmail.com>
2020-10-04 17:06:33 +0300

    updates on: Sunday, Oct 04, 2020

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-'use strict'
-
-var app = require('./index.js')
+import app from './index.js'
+const port = process.env.PORT || 8080
 
 require('greenlock-express')
   .init({
@@ -14,7 +13,32 @@ require('greenlock-express')
 
     // whether or not to run at cloudscale
     cluster: false
-  })
+  }).ready(httpsWorker(app))
 // Serves on 80 and 443
 // Get's SSL certificates magically!
-  .serve(app)
+// .serve(app)
+
+function httpsWorker (glx) {
+  //
+  // HTTPS 1.1 is the default
+  // (HTTP2 would be the default but... https://github.com/expressjs/express/issues/3388)
+  //
+
+  // Get the raw https server:
+  var httpsServer = glx.httpsServer(null, function (req, res) {
+    res.end('Hello, Encrypted World!')
+  })
+
+  httpsServer.listen(port, '0.0.0.0', function () {
+    console.info('Listening on ', httpsServer.address())
+  })
+
+  // Note:
+  // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+  // (the ACME and http->https middleware are loaded by glx.httpServer)
+  var httpServer = glx.httpServer()
+
+  httpServer.listen(port, '0.0.0.0', function () {
+    console.info('Listening on ', httpServer.address())
+  })
+}
