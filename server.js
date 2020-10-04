a58990f2f40b95dd229f23a02644052b8431f787
Abraham Bekele <abb2007hu@gmail.com>
2020-10-04 18:46:30 +0300

    updates on: Sunday, Oct 04, 2020

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function httpsWorker (glx) {
   // Get the raw https server:
   var httpsServer = glx.httpsServer(null, app)
 
-  httpsServer.listen(port, 'machinery-api.herokuapp.com', function () {
+  httpsServer.listen(port, '0.0.0.0', function () {
     console.info('Listening on ', httpsServer.address())
   })
 
@@ -36,7 +36,7 @@ function httpsWorker (glx) {
   // (the ACME and http->https middleware are loaded by glx.httpServer)
   var httpServer = glx.httpServer()
 
-  httpServer.listen(8080, 'machinery-api.herokuapp.com', function () {
+  httpServer.listen(8080, '0.0.0.0', function () {
     console.info('Listening on ', httpServer.address())
   })
 }
