2a1f2098d67ae57a9eb04c3401d35f4375227267
Abraham Bekele <abb2007hu@gmail.com>
2020-10-06 09:59:35 +0300

    update on Tuesday, Oct 6, 2020

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import apiRouter from './router/index'
 import { urlencoded, json } from 'body-parser'
 import authRouter from './router/auth/auth_router'
 import fileUpload from 'express-fileupload'
+import fs from 'fs'
+import path from 'path'
+import https from 'https'
+
 const app = express()
 const port = process.env.PORT || 8080
 const host = process.env.HOST || 'http://localhost'
@@ -45,5 +49,11 @@ app.use((err, req, res, next) => {
 })
 if (require.main === module) {
   app.listen(port, () => console.log(`listening on ${host}:${port}`))
+} else {
+  const httpsServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+  }, app)
+
+  httpsServer.listen(port, () => console.log(`a secured server is listening on ${port}`))
 }
-export default app
