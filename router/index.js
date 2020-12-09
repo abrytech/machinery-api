3e69e148838acdf31504479c6942a0abb1940221
Abraham Bekele <abb2007hu@gmail.com>
2020-12-09 21:38:46 +0300

    fix

--- router/index.js
+++ router/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import users from './users_router'
 import addresses from './addresses_router'
-// import files from './files_router'
+import pricebooks from './pricebook_router'
+import pricerates from './pricerate_router'
 import machines from './machines_router'
 import machineries from './machineries_router'
 import jobs from './jobs_router'
@@ -11,7 +12,8 @@ const router = Router()
 
 router.use('/addresses', addresses)
 router.use('/users', users)
-// router.use('/files', files)
+router.use('/pricebooks', pricebooks)
+router.use('/pricerates', pricerates)
 router.use('/machines', machines)
 router.use('/machineries', machineries)
 router.use('/jobs', jobs)
