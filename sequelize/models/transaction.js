b9ee7316a6ddec43e9cb7d7ad5762531503a47e0
Abraham Bekele <abb2007hu@gmail.com>
2020-12-28 18:01:37 +0300

    fix

--- sequelize/models/transaction.js
+++ sequelize/models/transaction.js
@@ -1,6 +1,6 @@
 'use strict'
-import { Model } from 'sequelize'
-export default (sequelize, DataTypes) => {
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
   class Transaction extends Model {
     /**
      * Helper method for defining associations.
