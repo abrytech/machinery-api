4e9f66a847fc16a28c464be2ffc18f2f161bc187
Abraham Bekele <abb2007hu@gmail.com>
2020-12-29 20:23:07 +0300

    fix

--- router/machineries_router.js
+++ router/machineries_router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { Op } from 'sequelize/types'
+import { Op } from 'sequelize'
 import { getParams, removeFields } from '../middleware/auth'
 import { deleteFileFromS3, uploadFileIntoS3 } from '../middleware/aws'
 import { Machinery, User, Machine, Picture } from '../sequelize/models'
