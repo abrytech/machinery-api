43017fc93c18b1c41321e4378bd5bbbb23d79f46
Abraham Bekele <abb2007hu@gmail.com>
2021-01-06 10:46:03 +0300

    fix

--- router/users_router.js
+++ router/users_router.js
@@ -170,7 +170,7 @@ router.get('', async (req, res) => {
 router.get('/:query', getParams, async (req, res, next) => {
   const params = req.queries
   const amount = await User.count()
-  if (req.userType === 'Lowbed Owner') params.where.userId = req.userId
+  if (req.userType === 'Lowbed Owner') params.where.id = req.userId
   if (req.userType === 'Machinery Owner') params.where.userType = 'Lowbed Owner'
   const users = await User.findAll({
     include,
